d6a0f42ba524b846eca8b571d4848ead4a435782
YokoMentor <91747328+YokoMentor@users.noreply.github.com>
2026-07-23 23:05:35 +0300

    Initial layout container

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 export default function Page() {
   return (
-    <div>Hello</div>
+    <div className='absolute top-11 md:top-17 flex flex-col justify-center items-center w-[328px] md:w-[540px]'>
+      <div className='flex justify-between items-baseline w-full mb-6 md:mb-8'>
+        <h1 className=''>TODO</h1>
+        <button className='w-[20px] h-[20px] md:w-[27px] md:h-[27px] bg-no-repeat bg-center bg-contain cursor-pointer'>button</button>
+      </div>
+    </div>
   );
 }
