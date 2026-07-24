45db3150080ccbd88d6ab16e001e16789236ad85
YokoMentor <91747328+YokoMentor@users.noreply.github.com>
2026-07-24 19:38:08 +0300

    Adjusted menu

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,18 @@ import styles from './page.module.css';
 
 export default function Page() {
   return (
-    <div className='absolute top-11 md:top-17 flex flex-col justify-center items-center w-[328px] md:w-[540px]'>
-      <div className='flex justify-between items-baseline w-full mb-6 md:mb-8'>
-        <div className={`${styles.logo} w-[28px] h-[32px]`}></div>
+    <div className='flex flex-col min-h-screen justify-center items-center'>
+      <div className='flex justify-between items-center text-center w-[327px]'>
+        <div className={`${styles.logo} w-[28px] h-[32px] bg-no-repeat bg-center`}></div>
         <div className='flex flex-row items-center justify-center'>
-          <input type="checkbox" id="toggle-switch" hidden/>
-          <label htmlFor="toggle-switch" className={`${styles.switch}`}></label>
+          <button className={`${styles.icon_arrow} w-[98px] h-[24px] bg-no-repeat bg-right text-[14px] font-bold pl-2`}>Serif</button>
+          <div className={`${styles.divider}`}></div>
+          <div className='flex flex-row items-center justify-center'>
+            <input type="checkbox" id="toggle-switch" hidden/>
+            <label htmlFor="toggle-switch" className={`${styles.switch}`}></label>
+          </div>
+          <button className={`${styles.icon_moon} w-[22px] h-[22px] bg-no-repeat bg-center bg-contain cursor-pointer ml-2`}></button>
         </div>
-        <button className={`${styles.icon_moon} w-[20px] h-[20px] bg-no-repeat bg-center bg-contain cursor-pointer`}></button>
       </div>
     </div>
   );
