072a93fd03587c4de0efb728ca7e7a6c3557fbff
YokoMentor <91747328+YokoMentor@users.noreply.github.com>
2026-07-25 01:05:21 +0300

    Noun container

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
           <div className={`${styles.logo} w-[30px] h-[34px] bg-no-repeat bg-center bg-contain`}></div>
           <div className='flex flex-row items-center justify-center'>
             <button className={`${styles.icon_arrow} w-[98px] h-[24px] bg-no-repeat bg-right text-[14px] font-bold pl-1`}>Serif</button>
-            <div className={`${styles.divider}`}></div>
+            <div className={`${styles.divider_v}`}></div>
             <div className='flex flex-row items-center justify-center'>
               <input type="checkbox" id="toggle-switch" hidden/>
               <label htmlFor="toggle-switch" className={`${styles.switch}`}></label>
@@ -26,6 +26,13 @@ export default function Page() {
             <div className='text-light-purple text-lg'>pronunciation</div>
           </div>
           <div className={`${styles.icon_play} w-[48px] h-[48px] bg-no-repeat bg-center bg-contain`}></div>
+        </div>
+        <div className='flex flex-col items-center w-full'>
+          <div className='flex flex-row justify-between items-center'>
+            <div className='font-bold text-lg italic mr-4'>noun</div>
+            <div className={`${styles.divider_h}`}></div>
+          </div>
+          <div className=''>Meaning</div>
         </div>
       </div>
     </div>
