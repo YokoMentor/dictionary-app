5ba771a1a274b94b25112735efae6b0393779fc4
YokoMentor <91747328+YokoMentor@users.noreply.github.com>
2026-07-24 23:48:44 +0300

    Pronunciation container

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
         <div className='flex justify-between items-center text-center w-full'>
           <div className={`${styles.logo} w-[30px] h-[34px] bg-no-repeat bg-center bg-contain`}></div>
           <div className='flex flex-row items-center justify-center'>
-            <button className={`${styles.icon_arrow} w-[98px] h-[24px] bg-no-repeat bg-right text-[14px] font-bold pl-2`}>Serif</button>
+            <button className={`${styles.icon_arrow} w-[98px] h-[24px] bg-no-repeat bg-right text-[14px] font-bold pl-1`}>Serif</button>
             <div className={`${styles.divider}`}></div>
             <div className='flex flex-row items-center justify-center'>
               <input type="checkbox" id="toggle-switch" hidden/>
@@ -18,8 +18,15 @@ export default function Page() {
         </div>
         <form className='flex justify-between items-center w-full rounded-2xl h-[48px] bg-search-bar w-full mt-6'>
           <input  type="text" placeholder='keyboard' className='text-xs focus:outline-none ml-6 font-bold placeholder:text-base placeholder:text-dark-heading'/>
-          <div className={`${styles.icon_search} w-[16px] h-[16px] bg-no-repeat bg-center bg-contain mr-6`}></div>
+          <div className={`${styles.icon_search} w-[18px] h-[18px] bg-no-repeat bg-center bg-contain mr-6`}></div>
         </form>
+        <div className='flex flex-row justify-between items-center w-full mt-5'>
+          <div className='flex flex-col text-left'>
+            <div className='font-bold text-[32px]'>keyboard</div>
+            <div className='text-light-purple text-lg'>pronunciation</div>
+          </div>
+          <div className={`${styles.icon_play} w-[48px] h-[48px] bg-no-repeat bg-center bg-contain`}></div>
+        </div>
       </div>
     </div>
   );
