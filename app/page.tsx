71865692b6c74e4dfc39fe55b1498a4c362cc70c
YokoMentor <91747328+YokoMentor@users.noreply.github.com>
2026-07-25 22:29:40 +0300

    Custom bullet points

--- app/page.tsx
+++ app/page.tsx
@@ -33,8 +33,8 @@ export default function Page() {
             <div className={`${styles.divider_h}`}></div>
           </div>
           <div className='mt-6'>Meaning</div>
-          <ul className={`${styles.ul} mt-5 pl-4`}>
-            <li>(etc.) A set of keys used to operate a typewriter, computer etc.</li>
+          <ul className={`${styles.ul}`}>
+            <li className={`${styles.li}`}>(etc.) A set of keys used to operate a typewriter, computer etc.</li>
             <li>A component of many instruments including the piano, organ, and harpsichord consisting of usually black and white keys that cause different tones to be produced when struck.</li>
             <li>A device with keys of a musical keyboard, used to control electronic sound-producing devices which may be built into or separate from the keyboard device.</li>
           </ul>
