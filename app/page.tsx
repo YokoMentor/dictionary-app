aa4edeaa763bd82b42ec4f89068a5ae94396686c
YokoMentor <91747328+YokoMentor@users.noreply.github.com>
2026-08-05 12:23:21 +0300

    Dark theme for Menu

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import styles from './page.module.css';
 export default function Page() {
   const darkBgStyle = 'bg-dark';
   const lightBgStyle = 'bg-light';
+  const darkMenuStyle = 'bg-dark-search text-light';
+  const lightMenuStyle = 'bg-light text-dark-heading';
   const darkMoonStyle = 'stroke-light-purple';
   const lightMoonStyle = 'stroke-light-heading';
   const darkTxtStyle = 'text-dark';
@@ -22,6 +24,16 @@ export default function Page() {
   const [wordError, setWordError] = useState(false);
   const [isVisible, setIsVisible] = useState(false);
   const [isMsgVisible, setIsMsgVisible] = useState(false);
+
+  const [dropDownMenuIsVisible, setDropDownMenuIsVisible] = useState(false);
+
+  function handleMenuClick() {
+    if(!dropDownMenuIsVisible) {
+      setDropDownMenuIsVisible(true);
+    } else {
+      setDropDownMenuIsVisible(false);
+    }
+  }
 
   function handleThemeSwitcher() {
     if (themeSwitch) {
@@ -66,7 +78,7 @@ export default function Page() {
           <div className='flex justify-between items-center text-center w-full mt-6 md:mt-14'>
             <div className={`${styles.logo} w-[30px] h-[34px] md:w-[34px] md:h-[38px] bg-no-repeat bg-center bg-contain`}></div>
             <div className='flex flex-row items-center justify-center'>
-              <button className={`${styles.icon_arrow} ${lightTheme ? darkTxtStyle : lightTxtStyle} w-[98px] h-[24px] bg-no-repeat bg-right text-[14px] md:text-[18px] font-bold pl-1 md:pr-1`}>Serif</button>
+              <button className={`${styles.icon_arrow} ${lightTheme ? darkTxtStyle : lightTxtStyle} w-[98px] h-[24px] bg-no-repeat bg-right text-[14px] md:text-[18px] font-bold pl-1 md:pr-1 cursor-pointer`} onClick={handleMenuClick}>Serif</button>
               <div className='h-[32px] w-[1px] bg-light-divider mx-4 md:mx-6'></div>
               <div className='flex flex-row items-center justify-center mr-2 md:mr-5'>
                 <input type="checkbox" id="theme-toggle" hidden/>
@@ -75,13 +87,13 @@ export default function Page() {
               <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22"><path fill="none" className={`${lightTheme ? lightMoonStyle : darkMoonStyle}`} strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M1 10.449a10.544 10.544 0 0 0 19.993 4.686C11.544 15.135 6.858 10.448 6.858 1A10.545 10.545 0 0 0 1 10.449Z"/></svg>
             </div>
           </div>
-          <div className='flex justify-center items-center relative z-2'>
-            <div className={`${styles.shadow} flex flex-col justify-center items-start absolute top-0 w-[122px] h-[152px] md:w-[182px] md:h-[152px] bg-light rounded-2xl ml-4 md:ml-72 mt-2 md:mt-3 pl-4 md:pl-6 font-bold text-[15px] md:text-[17px]`}>
-              <div>Sans Serif</div>
-              <div className='my-4'>Serif</div>
-              <div>Mono</div>
+          {dropDownMenuIsVisible && <div className='flex justify-center items-center relative z-2'>
+            <div className={`${lightTheme ? styles.menu_shadow_light : styles.menu_shadow_dark} ${lightTheme ? lightMenuStyle : darkMenuStyle} flex flex-col justify-center items-start absolute top-0 w-[122px] h-[152px] md:w-[182px] md:h-[152px] rounded-2xl ml-4 md:ml-72 mt-2 md:mt-3 pl-4 md:pl-6 font-bold text-[15px] md:text-[17px]`}>
+              <div className='cursor-pointer hover:text-light-purple'>Sans Serif</div>
+              <div className='cursor-pointer hover:text-light-purple my-4'>Serif</div>
+              <div className='cursor-pointer hover:text-light-purple'>Mono</div>
             </div>
-          </div>
+          </div>}
           <div className='flex flex-col w-full text-left'>
             <form className='flex justify-between items-center w-full h-[48px] md:h-[64px] mt-6 md:mt-14 relative z-1' onSubmit={handleSearch}>
               <label htmlFor="word"></label>
